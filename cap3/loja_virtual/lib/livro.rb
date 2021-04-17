e2018bd87b494231494d9146ac3c8d3b75bfdfef
Alan Oliveira <alancjobm@gmail.com>
2021-04-17 00:32:49 -0300

    sobrescrevendo o metodo to_s

--- cap3/loja_virtual/lib/livro.rb
+++ cap3/loja_virtual/lib/livro.rb
@@ -1,8 +1,15 @@
+# coding: utf-8
 class Livro
-  def initialize(autor, isbn = "1", numero_de_paginas)
+  def initialize(autor, isbn = "2", numero_de_paginas)
     @autor = autor
-    @isb = isbn
+    @isbn = isbn
     @numero_de_paginas = numero_de_paginas
   end
+
+  # sobrescrevendo o método da classe Object to_s (representação de uma instância)
+  def to_s
+    "Autor: #{@autor}, ISBN: #{@isbn}, Páginas: #{@numero_de_paginas}"
+  end
+
 end
 
--- cap3/readme.md
+++ cap3/readme.md
@@ -1,3 +1,4 @@
 # Capítulo 3 - O começo da nossa aplicação
 
 ### 3.3 Defina os Atributos de Instância - 35
+### 3.4 Sobrescrevendo o Método `to_s`
