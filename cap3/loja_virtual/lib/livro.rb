4aea9301cd73b5279d978abdf0154f4b8daa4090
Alan Oliveira <alancjobm@gmail.com>
2021-04-17 00:11:28 -0300

    definindo atributos na instancia

--- cap3/loja_virtual/lib/livro.rb
+++ cap3/loja_virtual/lib/livro.rb
@@ -1,6 +1,8 @@
 class Livro
   def initialize(autor, isbn = "1", numero_de_paginas)
-    puts "Autor: #{autor}, ISBN: #{isbn}, Pág: #{numero_de_paginas}"
+    @autor = autor
+    @isb = isbn
+    @numero_de_paginas = numero_de_paginas
   end
 end
 
--- cap3/readme.md
+++ cap3/readme.md
@@ -1,1 +1,3 @@
 # Capítulo 3 - O começo da nossa aplicação
+
+### 3.3 Defina os Atributos de Instância - 35
